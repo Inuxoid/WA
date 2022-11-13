efb328c6372b133e53abe5c363eacc9d5a2089a7
Inuxoid <alpha.inuxoid@gmail.com>
2022-11-13 23:25:32 +0300

    Del

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,13 +38,6 @@ namespace WA.Controllers
         public string NewOrder(string? Adress, string? Person)
         {
             return "Thanks for the order, " + Person + ", we sent all information to " + Adress;
-        }        
-        
-        
-        [Route("home/postfeedback")]
-        public string PostFeedback(string? Adress, string? Person, string? Feedback)
-        {
-            return $"Hello, + {Person}!" + "Thanks for your feedback.";
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
